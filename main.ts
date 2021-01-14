b9cd290b84074169081551afff02491bc44f3bbd
Conectabot <77430443+Conectabot@users.noreply.github.com>
2021-01-14 08:18:44 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let mano = 0
+input.onGesture(Gesture.Shake, function () {
+    mano = randint(1, 3)
+    if (mano == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (mano == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
